39a1259e24d95416691b0b76a83111b4be88fc09
ahansrisuk <ahansrisuk@gmail.com>
2020-04-01 16:18:43 -0400

    fixed badge bug, conditional back button

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@
 		<hr class="border-t-2 border-main mt-2">
 		<div class="flex items-center">
 			<SearchBar class="my-8 flex-grow"/>
-			<router-link to="/inventory" v-if="onHomePage">
+			<router-link to="/inventory" v-show="onHomePage">
 				<Backpack />
 			</router-link>
-            <router-link to="/" v-else class="ml-2">
+            <router-link to="/" v-show="!onHomePage" class="ml-2">
                 Back
             </router-link>
 		</div>
