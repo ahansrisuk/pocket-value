c2ec0b3846a28250910eeef4bc34d35bd9e6f7d0
ahansrisuk <ahansrisuk@gmail.com>
2020-04-01 17:54:02 -0400

    implemented autocomplete

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="max-w-sm">
-    <h1>Pocket Value</h1>
-    <div class="flex justify-center">
-        <p class="text-sm">for Animal Crossing: New Horizons</p>
+    <div class="max-w-sm">
+        <router-link to="/">
+            <h1>Pocket Value</h1>
+            <div class="flex justify-center">
+                <p class="text-sm">for Animal Crossing: New Horizons</p>
+            </div>
+        </router-link>
     </div>
-  </div>
 </template>
 
 <script>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,12 +1,29 @@
 <template>
-    <div class="border">
-        <div class="flex p-1">
-            <img src="../assets/search.svg" alt="search" />
-            <input 
-                type="text" 
-                class="bg-background focus:outline-none ml-3"
-                v-model="searchInput" 
-            >
+    <div class="relative">
+        <div class="border">
+            <div class="flex p-1">
+                <img src="../assets/search.svg" alt="search" />
+                <input 
+                    type="text" 
+                    class="bg-background focus:outline-none ml-3 w-full"
+                    v-model="searchInput" 
+                >
+            </div>
+        </div>
+        <div class="overflow-auto absolute bg-background mt-2 border w-3/4" style="max-height: 400px" v-show="itemSuggestions.length > 0">
+            <div 
+                v-for="item in itemSuggestions" 
+                :key="item.id"
+                class="flex w-full items-center justify-between border-main border-t">
+                <div class="flex items-center">
+                    <img :src="item.image_path" height="50px" width="50px" alt="fish"/>
+                    <p class="text-sm ml-4">{{ item.name }}</p>
+                </div>
+                <div class="flex items-center">
+                    <p class="text-sm">{{ item.value }}</p>
+                    <button class="ml-4 mr-1 bg-mustard px-2 rounded-lg" @click="addItemToInventory(item)">+</button>        
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -27,5 +44,10 @@ export default {
             } )
         },
     },
+    methods: {
+        addItemToInventory (item) {
+            this.$store.commit('addItemToInventory', item)
+        }
+    }
 }
 </script>
