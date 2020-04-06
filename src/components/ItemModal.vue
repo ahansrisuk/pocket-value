0d7437c155f24c013c907251ffb884f945cd90b3
ahansrisuk <ahansrisuk@gmail.com>
2020-04-06 15:30:49 -0400

    basic card styling

--- src/components/ItemModal.vue
+++ src/components/ItemModal.vue
@@ -1,12 +1,27 @@
 <template>
     <div class="fixed h-full w-full top-0 right-0 flex">
         <div class="fixed h-full w-full bg-black opacity-50"></div>
-        <div class="bg-background p-20 m-auto z-10 border">
-            {{ item.name }}
-            <button class="ml-4 mr-1 bg-mustard px-2 rounded-lg" @click="addItemToInventory(item)">+</button>
-            <button class="ml-4 mr-1 bg-mustard px-2 rounded-lg" @click="$emit('close-modal')">
-                <img src="../assets/close.svg" alt="close" />
-            </button>        
+        <div class="bg-background p-4 m-auto z-10 border w-1/3">
+            <div class="flex justify-between items-center">
+                <div class="flex">
+                    <h2>{{ item.name }}</h2>
+                    <button class="ml-4 bg-mustard px-3 rounded-lg text-xl" @click="addItemToInventory(item)">+</button>
+                </div>
+                <button class="px-4" @click="$emit('close-modal')">
+                    <img src="../assets/close.svg" alt="close">
+                </button>
+            </div>
+            <hr class="border-t-2 border-main mt-2">
+            <div class="flex items-center">
+                <img :src="item.image_path" alt="item.name" height="90px" width="90px">
+                <div class="ml-20">
+                    <p>{{ item.value }} bells </p>
+                    <p>Found in {{ item.location }} </p>
+                    <p>{{ item.time }} </p>
+                </div>
+            </div>
+                {{ item.northern_months }}
+                {{ item.southern_months }}
         </div>
       
     </div>
