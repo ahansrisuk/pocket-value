6db39f7278aebf592166a69c5b672ae78ce4e0da
ahansrisuk <ahansrisuk@gmail.com>
2020-04-06 14:47:43 -0400

    open and close modal

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,23 +1,38 @@
 <template>
     <div>
-        <div class="flex w-full items-center justify-between border-main border-t">
-            <div class="flex items-center">
+        <div class="flex w-full items-center justify-between border-main border-t" 
+            @click="showModal = true"
+            >
+            <button class="flex items-center flex-grow outline-none">
                 <img :src="item.image_path" height="50px" width="50px" alt="fish"/>
                 <p class="text-sm ml-4">{{ item.name }}</p>
-            </div>
+            </button>
             <div class="flex items-center">
                 <p class="text-sm">{{ item.value }}</p>
                 <button class="ml-4 mr-1 bg-mustard px-2 rounded-lg" @click="addItemToInventory(item)">+</button>        
             </div>
         </div>
+        <ItemModal 
+            v-if="showModal" 
+            @close-modal="showModal = false"
+            :item="item" 
+        />
     </div>
 </template>
 
 <script>
 
+import ItemModal from './ItemModal';
+
 export default {
     name: 'Item',
+    components: { ItemModal },
     props: ['item'],
+    data: function () {
+        return {
+            showModal: false,
+        }
+    },
     methods: {
         addItemToInventory (item) {
             this.$store.commit('addItemToInventory', item)
---
+++ src/components/ItemModal.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="fixed h-full w-full top-0 right-0 flex">
+        <div class="fixed h-full w-full bg-black opacity-50"></div>
+        <div class="bg-background p-20 m-auto z-10 border">
+            {{ item.name }}
+            <button class="ml-4 mr-1 bg-mustard px-2 rounded-lg" @click="addItemToInventory(item)">+</button>
+            <button class="ml-4 mr-1 bg-mustard px-2 rounded-lg" @click="$emit('close-modal')">
+                <img src="../assets/close.svg" alt="close" />
+            </button>        
+        </div>
+      
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ItemModal',
+    props: ['item'],
+    methods: {
+        addItemToInventory (item) {
+            this.$store.commit('addItemToInventory', item)
+        },
+    }
+
+}
+</script>
