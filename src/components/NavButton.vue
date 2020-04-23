d16a091d7ad0684bca8ecdacdcfafcf264dfb27a
Allen Hansrisuk <ahansrisuk@email.com>
2020-04-23 14:23:35 -0400

    refactored nav buttons

---
+++ src/components/NavButton.vue
@@ -0,0 +1,23 @@
+<template>
+<router-link :to="destination"> 
+    <div class="border rounded flex px-2 py-1 items-center mb-2 justify-between w-custom-1 md:w-custom-2">
+        <span class="text-xs">{{ name }}</span>
+        <slot></slot>
+    </div>
+</router-link>
+</template>
+
+<script>
+export default {
+    name: 'NavButton',
+    props: [
+        'name',
+        'image',
+        'destination'
+    ]
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,22 +1,29 @@
 <template>
 <div>
-    <section class="flex mb-4 justify-between flex-wrap">
+    <section class="flex mb-2 justify-between flex-wrap">
 		<div class="border rounded flex px-2 py-1 items-center mb-2 justify-between w-custom-1 md:w-custom-2">
 			<span class="text-xs">North Hemisphere</span>
-			<img src="../assets/location-icon.svg" alt="world" height="24px" width="24px"/>
+			<img src="../assets/location-icon.svg" alt="pin" height="24px" width="24px"/>
 		</div>
-		<div class="border rounded flex px-2 py-1 items-center mb-2 justify-between w-custom-1 md:w-custom-2">
-			<span class="text-xs">Available Now</span>
-			<img src="../assets/calendar.svg" alt="world" height="24px" width="24px" />
-		</div>
-		<div class="border rounded flex px-2 py-1 items-center mb-2 justify-between w-custom-1 md:w-custom-2">
-			<span class="text-xs">Outgoing Critters</span>
-			<img src="../assets/calendar.svg" alt="world" height="24px" width="24px" />
-		</div>
-		<div class="border rounded flex px-2 py-1 items-center mb-2 justify-between w-custom-1 md:w-custom-2">
-			<span class="text-xs">Incoming Critters</span>
-			<img src="../assets/calendar.svg" alt="world" height="24px" width="24px" />
-		</div>
+
+		<NavButton 
+			name="Available Now" 
+			destination="/available-now"	
+			>
+			<img src="../assets/calendar.svg" alt="calendar" height="24px" width="24px" />
+		</NavButton>
+		<NavButton 
+			name="Outgoing Critters" 
+			destination="/outgoing-critters"	
+			>
+			<img src="../assets/pink.svg" alt="pink circle" height="24px" width="24px" class="p-1" />
+		</NavButton>
+		<NavButton 
+			name="Incoming Critters" 
+			destination="/incoming-critters"	
+			>
+			<img src="../assets/blue.svg" alt="blue circle" height="24px" width="24px" class="p-1" />
+		</NavButton>
     </section>
     <section>
 		<ItemTypeCard class="mb-4" type="Fish"/>
@@ -28,11 +35,13 @@
 <script>
 
 import ItemTypeCard from '../components/ItemTypeCard';
+import NavButton from '../components/NavButton';
 
 export default {
   name: 'App',
   components: {
-    ItemTypeCard,
+	ItemTypeCard,
+	NavButton
   },
   created() {
     this.$store.dispatch('loadItems');
