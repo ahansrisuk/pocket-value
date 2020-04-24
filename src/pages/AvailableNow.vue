6ee30ba5aeaf92facd1b5a3c63d23ce8a49523c4
Allen Hansrisuk <ahansrisuk@email.com>
2020-04-24 13:25:06 -0400

    all page headers

--- src/pages/AvailableNow.vue
+++ src/pages/AvailableNow.vue
@@ -1,19 +1,22 @@
 <template>
-    <section>
+    <section class="border">
+        <PageHeader title="Available Now" />
         <Item v-for="item in currentlyAvailableItems" :key="item.id"
             :item="item"
             :addItemButton="true"
+            class="px-2"
         />
     </section>
 </template>
 
 <script>
 
-import Item from '../components/Item'
+import Item from '../components/Item';
+import PageHeader from '../components/PageHeader';
 
 export default {
     name:'AvailableNow',
-    components: { Item },
+    components: { Item, PageHeader },
     computed: {
         currentlyAvailableItems() {
             const date = new Date();
