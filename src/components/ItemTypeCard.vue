d3cc9fc2060d727fc02016988b64e8aee90542b9
Allen Hansrisuk <ahansrisuk@email.com>
2020-05-01 15:02:58 -0400

    make whole type box clickable, fix arrows

--- src/components/ItemTypeCard.vue
+++ src/components/ItemTypeCard.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="border px-2">
-        <div class="flex justify-between items-center">
-            <div class="flex items-center">
-                <img v-if="items[0].image_path" :src="previewImage" height="50px" width="50px" alt="fish"/>
-                <h2 class="ml-4">{{ type }}</h2>
+    <div class="border px-2 flex flex-col">
+        <button @click="handleClick" class="w-full">
+            <div class="flex justify-between items-center">
+                <div class="flex items-center">
+                    <img v-if="items[0].image_path" :src="previewImage" height="50px" width="50px" alt="fish"/>
+                    <h2 class="ml-4">{{ type }}</h2>
+                </div>
+                <Arrow class="arrow" />
             </div>
-            <button @click="open = !open">
-                <Arrow />
-            </button>
-        </div>
+        </button>
         <div v-show="open" class="overflow-auto" style="max-height: 400px">
             <Item :item="item" v-for="item in items" :key="item.id" :addItemButton="true"/>
         </div>
@@ -19,6 +19,7 @@
 
 import Arrow from './arrow';
 import Item from './Item';
+import anime from 'animejs';
 
 export default {
     name: 'ItemTypeCard',
@@ -38,7 +39,48 @@ export default {
     data: function () {
         return {
             open: false,
+            openAnimation: '',
+            closeAnimation: '',
         }
     },
+    methods: {
+        handleClick (e) {
+            if (!this.open) {
+                this.openArrow(e.target.querySelector('.arrow'));
+            } else {
+                this.closeArrow(e.target.querySelector('.arrow'));
+
+            }
+            this.open = !this.open;
+        },
+        openArrow (el, done) {
+            anime({
+                targets: el,
+                rotateX: [0, 180],
+                easing: 'easeInOutSine',
+                duration: 200,
+                complete: done,
+            })
+        },
+        closeArrow (el, done) {
+            anime({
+                targets: el,
+                rotateX: [180, 0],
+                easing: 'easeInOutSine',
+                duration: 200,
+                complete: done,
+            })
+        }
+    },
+    mounted() {
+        this.openAnimation = 
+            anime({
+                targets: ".arrow",
+                rotateX: [0, 180],
+                easing: 'easeInOutSine',
+                duration: 200,
+                autoplay: false,
+            })
+    }
 }
 </script>
--- src/components/arrow.vue
+++ src/components/arrow.vue
@@ -5,53 +5,15 @@
         viewBox="0 0 22 14" 
         fill="none" 
         xmlns="http://www.w3.org/2000/svg" 
-        @click="handleClick">
+    >
         <path d="M2.585 0.207504L11 8.60417L19.415 0.207504L22 2.7925L11 13.7925L0 2.7925L2.585 0.207504Z" fill="#726A5F"/>
     </svg>
 </template>
 
 <script>
-import anime from 'animejs';
 
 export default {
     name: 'Arrow',
-    data: function () {
-        return {
-            openAnimation: '',
-            closeAnimation: '',
-            open: false
-        }
-    },
-    methods: {
-        handleClick () {
-            if (!this.open) {
-                this.openAnimation.play();
-            } else {
-                this.closeAnimation.play();
-            }
-
-            this.open = !this.open;
-        }
-    },
-    mounted() {
-        const targets = this.$el;
-        this.closeAnimation = 
-            anime({
-                targets,
-                rotateX: [180, 0],
-                easing: 'easeInOutSine',
-                duration: 200,
-                autoplay: false,
-            })
-        this.openAnimation = 
-            anime({
-                targets,
-                rotateX: [0, 180],
-                easing: 'easeInOutSine',
-                duration: 200,
-                autoplay: false,
-            })
-    }
 }
 
 </script>
